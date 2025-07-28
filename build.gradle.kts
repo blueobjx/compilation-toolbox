3ec7ccafcb177c755b1e0fa7f2cbdd40212c6044
Doug McClure <dmcclure@biznuvo.com>
2025-07-27 22:09:05 -0400

    Initial version

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("project-report")
+}
+
+group = "blueobjx"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:11.0.2")
+
+    testImplementation("javax.inject:javax.inject:1")
+    testImplementation("org.testng:testng:5.14.10")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/blueobjx/compilation-toolbox")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "compilation-toolbox"
